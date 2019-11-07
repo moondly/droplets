49327001c78a15a4096c88258404001710623c41
moondly <moondly@protonmail.com>
2019-11-07 09:12:55 +0100

    Add swarmGluster droplet

---
+++ drops/swarmGluster/infra.tf
@@ -0,0 +1,53 @@
+provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
+}
+
+
+resource "lxd_container" "swarm01" {
+  name      = "swarm01"
+  image     = "ubuntu:18.04"
+  ephemeral = false
+  profiles  = ["default"]
+  limits = {
+    cpu    = "2"
+    memory = "1024MB"
+  }
+
+  config = {
+    "user.access_interface" = "eth0"
+  }
+
+}
+
+resource "lxd_container" "swarm02" {
+  name      = "swarm02"
+  image     = "ubuntu:18.04"
+  ephemeral = false
+  profiles  = ["default"]
+  limits = {
+    cpu    = "2"
+    memory = "1024MB"
+  }
+
+  config = {
+    "user.access_interface" = "eth0"
+  }
+
+}
+
+resource "lxd_container" "swarm03" {
+  name      = "swarm03"
+  image     = "ubuntu:18.04"
+  ephemeral = false
+  profiles  = ["default"]
+  limits = {
+    cpu    = "2"
+    memory = "1024MB"
+  }
+
+  config = {
+    "user.access_interface" = "eth0"
+  }
+
+}
